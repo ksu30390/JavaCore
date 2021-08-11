49420bddf804670abe6bdbb7f1dcdd1ff5ff6edd
ksenia_nikulina <testerksu@gmail.com>
2021-08-11 06:38:55 +0300

    New LessonTwo

---
+++ LessonTwo/MyArraySizeException.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.JavaCoreLesson2;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException() {
+        super();
+    }
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
