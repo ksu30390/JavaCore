f610fae8cea0fc1b51bcf75e0eefaa59d7745c0b
ksenia_nikulina <testerksu@gmail.com>
2021-08-23 23:12:51 +0300

    Lesson 6 is added

---
+++ CalgaryForecast.java
@@ -0,0 +1,50 @@
+package ru.geekbrains.JavaCoreLesson6;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class CalgaryForecast {
+
+    public static void main(String[] args) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host("dataservice.accuweather.com")
+                .addPathSegment("forecasts")
+                .addPathSegment("v1")
+                .addPathSegment("daily")
+                .addPathSegment("5day")
+                .addPathSegment("52479")
+                .addQueryParameter("apikey", "70Hl5xKPmRRZSnBX7JFGfm16nHbQhgAL")
+                .build();
+
+        // Экземпляр класса Request создается через Builder (см. паттерн проектирования "Строитель")
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        // Получение объекта ответа от сервера
+        Response response = client.newCall(request).execute();
+
+        // Тело сообщения возвращается методом body объекта Response
+        String body = response.body().string();
+
+        System.out.println(response.code());
+        System.out.println(response.headers());
+        System.out.print(body);
+
+
+
+
+
+
+
+
+    }
+}
